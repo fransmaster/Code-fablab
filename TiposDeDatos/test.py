f544fdca925dc0785d9f92109730e0181e39363b
fransmaster <jairgue00@gmail.com>
2022-06-15 19:11:32 -0400

    tipos de datos

---
+++ TiposDeDatos/test.py
@@ -0,0 +1,52 @@
+nombre = 'fransmaster3000'
+x,y,z = 10, 3, 4
+
+print(nombre)
+print(z,x,y)
+
+print(type(y))
+x ='real madrid campeon el mejor equipo del mundo'
+
+print (x)
+print(x)
+x = ''' real madrid 
+el mejor equipo del mundo'''
+
+
+
+x = 'francis'
+y ='guerra'
+z = x + y
+print(z)
+print(len(x))
+print(x.upper())
+x ='cr{}'
+y = 700000
+print (x.format(y))
+print (100>=100)
+x = ['francis',22,False]
+print(x)
+x[0]='leo'
+
+#esto es un comentario y la idea es que apuntes que hace cada vaina 
+
+print('Mi nombre es ' + x[0])
+x.append('real madrid')
+print(x)
+x = ('a','b','c','d','e','f')
+print(x)
+y = list(x)
+print (y)
+
+
+x={'vinicius','rodrygo','benzema'}
+for jugador in x:
+	print (jugador)
+
+user = {
+	'name': 'leo'
+}
+
+x = user.get("name")
+
+print(x)
